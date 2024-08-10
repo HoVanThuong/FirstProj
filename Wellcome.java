7b64ab9519794070829b007fb2e11f512a39221a
Hồ Vạn Thương <23661861.thuong@student.iuh.edu.vn>
2024-08-10 20:16:36 +0700

    Delete Wellcome.java

--- Wellcome.java
+++
@@ -1,8 +0,0 @@
-public class Wellcome {
-    public static void main(String[] args) {
-        String name = "Ho Van Thuong";
-        String lopHocPhan = "DHKTPM19A";
-        int maSoSinhVien = 23661861;
-        System.out.printf("Em ten la %s\nMa so sinh vien: %s\nTen lop hoc phan la: %s", name, lopHocPhan, maSoSinhVien);
-    }
-}
